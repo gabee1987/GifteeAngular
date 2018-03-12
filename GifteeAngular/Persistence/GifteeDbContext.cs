6f1910f6447b3cc87b5bd194e5c67ac9d401105c
Gabor Koncz <gabee1987@gmail.com>
2018-03-12 15:02:37 +0100

    added GifteeDbContext.cs and added services to Startup.cs

---
+++ GifteeAngular/Persistence/GifteeDbContext.cs
@@ -0,0 +1,22 @@
+﻿using GifteeAngular.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GifteeAngular.Persistence
+{
+    public class GifteeDbContext : DbContext
+    {
+        #region DbSets
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Giftee> Giftees { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Gift> Gifts { get; set; }
+
+        #endregion
+
+        public GifteeDbContext(DbContextOptions<GifteeDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
--- GifteeAngular/Startup.cs
+++ GifteeAngular/Startup.cs
@@ -1,7 +1,10 @@
+using AutoMapper;
+using GifteeAngular.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -19,7 +22,14 @@ namespace GifteeAngular
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add Database context
+            var connString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=GifteeDB;Integrated Security=True;Connect Timeout=30;";
+            services.AddDbContext<GifteeDbContext>(options => options.UseSqlServer(connString));
+
+
+            // Add framework services
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddAutoMapper();
 
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
