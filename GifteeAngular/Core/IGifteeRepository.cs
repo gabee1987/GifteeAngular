c05c261a53abd414af64c25e111871b09141ee0a
Gabor Koncz <gabee1987@gmail.com>
2018-03-12 15:49:39 +0100

    added resource classes to giftee and user models and mapping profile for them, deleted unnecessary usings

--- GifteeAngular/Core/IGifteeRepository.cs
+++ GifteeAngular/Core/IGifteeRepository.cs
@@ -1,7 +1,4 @@
 ﻿using GifteeAngular.Core.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace GifteeAngular.Core
--- GifteeAngular/Core/IUserRepository.cs
+++ GifteeAngular/Core/IUserRepository.cs
@@ -1,7 +1,5 @@
 ﻿using GifteeAngular.Core.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace GifteeAngular.Core
